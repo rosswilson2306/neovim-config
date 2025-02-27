2b03007360b2366fef38ec4c09667787477f5802
Ross Wilson <ross.wilson@homeflow.co.uk>
2025-02-27 14:54:24 +0000

    add terraform as filetype for formatting with conform

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -37,6 +37,7 @@ return {
         -- python = { "isort", "black" },
         --
         tf = { "terraform-ls" },
+        terraform = { "terraform-ls" },
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { "prettierd", "prettier", stop_after_first = true },
         typescript = { "prettierd", "prettier", stop_after_first = true },
